92e2a6924b4b7f20d5d01bae39aa1b4e99136247
Steve Gardner <github@ste.vg>
2023-03-07 23:02:45 -0800

    created Those3DLetters

---
+++ src/letter.ts
@@ -0,0 +1,63 @@
+class That3DLetter {
+  private container: HTMLElement;
+  private mainElement: HTMLElement | null = null;
+  private elements: HTMLElement[] = [];
+
+  public character: string;
+  public index: number;
+
+  constructor(
+    letterString: string,
+    count: number,
+    container: HTMLElement,
+    index: number,
+  ) {
+    this.container = container;
+    this.character = letterString;
+
+    this.index = index;
+
+    this.createLayers(count);
+  }
+
+  private createLayers(count: number) {
+    for (let i = 0; i < count; i++) {
+      const layer = i;
+
+      const span = document.createElement('span');
+      span.setAttribute('aria-hidden', 'true');
+      span.classList.add('letter');
+      span.classList.add(i == 0 ? 'front' : 'under');
+      if (i == count - 1) span.classList.add('back');
+      span.innerHTML = this.character === ' ' ? '&nbsp;' : this.character;
+      span.dataset['depth'] = layer.toString();
+      span.dataset['index'] = this.index.toString();
+      span.dataset['character'] = this.character;
+      span.style.setProperty('--layer', (count - layer).toString());
+      span.style.setProperty(
+        '--centerOffset',
+        ((layer - (count - 1) * 0.5) / ((count - 1) * 0.5)).toString(),
+      );
+
+      this.elements.push(span);
+
+      if (i === 0) {
+        this.mainElement = span;
+      }
+
+      this.container.appendChild(span);
+    }
+  }
+
+  public resize() {
+    if (this.mainElement) {
+      const x = this.mainElement.offsetLeft;
+
+      this.elements.forEach((span) => {
+        span.style.setProperty('--xPos', x.toString());
+      });
+    }
+  }
+}
+
+export { That3DLetter };
--- src/word.ts
+++ src/word.ts
@@ -1,11 +1,15 @@
+import { That3DLetter } from './letter';
+
 class That3DWord {
   public element: HTMLElement;
   private wordString: string = '';
-  private letters: string[] = [];
-  private layers: number = 1;
+  private letters: That3DLetter[] = [];
+  private layers: number = 0;
 
-  constructor(element: HTMLElement) {
+  constructor(element: HTMLElement, layers?: number) {
     this.element = element;
+
+    if (layers) this.layers = layers;
     this.init();
   }
 
@@ -13,6 +17,30 @@ class That3DWord {
     this.wordString = this.element.innerHTML;
     this.element.innerHTML = '';
     this.element.setAttribute('aria-label', this.wordString);
+
+    const letterStrings = this.wordString.split('');
+    const style = getComputedStyle(this.element);
+    if (!this.layers) this.layers = Number(style.getPropertyValue('--layers'));
+
+    this.letters = letterStrings.map(
+      (letter, i) => new That3DLetter(letter, this.layers, this.element, i),
+    );
+  }
+
+  public reset() {
+    this.init();
+  }
+
+  public onResize() {
+    this.element.style.setProperty(
+      '--width',
+      this.element.clientWidth.toString(),
+    );
+    this.element.style.setProperty(
+      '--height',
+      this.element.clientHeight.toString(),
+    );
+    this.letters.forEach((letter) => letter.resize());
   }
 
   public get word() {
